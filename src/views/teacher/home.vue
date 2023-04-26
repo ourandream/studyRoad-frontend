1b134bbc75c612530c2ab97cf68b2e09d92957c7
ourandream <ouranhuang@outlook.com>
2023-04-26 11:15:02 +0800

    chore: 调整了一下头像框和menu顺序

--- src/views/teacher/home.vue
+++ src/views/teacher/home.vue
@@ -8,60 +8,67 @@
             display: flex;
             margin: 4vh 0px;
             align-items: center;
-            padding-left: 12%;
             font-size: large;
             text-decoration: none;
             cursor: pointer;
           "
           @click="Return"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            fill="none"
-            version="1.1"
-            style="margin-right: 13%"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
+          <el-dropdown
+            style="
+              display: flex;
+              justify-content: center;
+              width: 100%;
+              flex-direction: column;
+              align-items: center;
+            "
           >
-            <defs>
-              <linearGradient
-                x1="-5.551115123125783e-17"
-                y1="-1.1102230246251565e-16"
-                x2="1"
-                y2="1"
-                id="master_svg0__14"
+            <el-image
+              :src="teaimgurl"
+              style="
+                width: 3vw;
+                border-radius: 50%;
+                background-color: #8db1f1;
+                margin: 0px 0vh 0px 0px;
+              "
+            ></el-image>
+            <div style="padding-top: 1.5vh; color: white">{{ name }}</div>
+
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item
+                ><span
+                  class="zc-iconshouye"
+                  @click="Return"
+                  style="font-size: 15px"
+                  >&nbsp;回到首页</span
+                >
+              </el-dropdown-item>
+              <el-dropdown-item
+                ><span
+                  class="el-icon-user-solid"
+                  @click="Info"
+                  style="font-size: 15px"
+                  >&nbsp;信息维护</span
+                ></el-dropdown-item
               >
-                <stop
-                  offset="0%"
-                  stop-color="#FFFFFF"
-                  stop-opacity="0.699999988079071"
-                />
-                <stop offset="100%" stop-color="#FFFFFF" stop-opacity="1" />
-              </linearGradient>
-            </defs>
-            <g style="mix-blend-mode: passthrough">
-              <g style="mix-blend-mode: passthrough">
-                <ellipse
-                  cx="16"
-                  cy="16"
-                  rx="16"
-                  ry="16"
-                  fill="#3751FF"
-                  fill-opacity="1"
-                />
-              </g>
-              <g style="mix-blend-mode: passthrough">
-                <path
-                  d="M11,10C11,9.447716,11.447715,9,12,9C12,9,15.99048,9,15.99048,9C18.212699999999998,9,19.933329999999998,9.609551,21.1524,10.82865C22.3841,12.04775,23,13.764980000000001,23,15.98034C23,18.2088,22.3841,19.9391,21.1524,21.171300000000002C19.933329999999998,22.3904,18.212699999999998,23,15.99048,23C15.99048,23,12,23,12,23C11.447715,23,11,22.552300000000002,11,22C11,22,11,10,11,10C11,10,11,10,11,10Z"
-                  fill="url(#master_svg0__14)"
-                  fill-opacity="1"
-                />
-              </g>
-            </g>
-          </svg>
-          学路系统
+              <el-dropdown-item
+                ><span
+                  class="el-icon-s-help"
+                  @click="changehead"
+                  style="font-size: 15px"
+                  >&nbsp;头像更换</span
+                ></el-dropdown-item
+              >
+              <el-dropdown-item
+                ><span
+                  class="el-icon-switch-button"
+                  @click="SignOut"
+                  style="font-size: 15px"
+                  >&nbsp;退出登录</span
+                ></el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </el-dropdown>
         </div>
 
         <el-menu
@@ -74,6 +81,11 @@
           unique-opened
           ref="menu"
         >
+          <el-menu-item index="/stuTimeline">
+            <i class="el-icon-time"></i>
+            <span slot="title">学路看板</span>
+          </el-menu-item>
+
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-user-solid"></i>
@@ -125,72 +137,11 @@
               <span slot="title">违纪扣分</span>
             </el-menu-item>
           </el-submenu>
-          <el-menu-item index="/stuTimeline">
-            <i class="el-icon-time"></i>
-            <span slot="title">学路看板</span>
-          </el-menu-item>
           <el-menu-item index="/evaluation">
             <i class="el-icon-s-data"></i>
             <span slot="title">素质报告统计</span>
           </el-menu-item>
         </el-menu>
-        <el-dropdown
-          style="
-            position: absolute;
-            bottom: 2vh;
-            display: flex;
-            justify-content: center;
-            width: 100%;
-            flex-direction: column;
-            align-items: center;
-          "
-        >
-          <el-image
-            :src="teaimgurl"
-            style="
-              width: 3vw;
-              border-radius: 50%;
-              background-color: #8db1f1;
-              margin: 0px 0vh 0px 0px;
-            "
-          ></el-image>
-          <div style="padding-top: 1.5vh">{{ name }}</div>
-
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item
-              ><span
-                class="zc-iconshouye"
-                @click="Return"
-                style="font-size: 15px"
-                >&nbsp;回到首页</span
-              >
-            </el-dropdown-item>
-            <el-dropdown-item
-              ><span
-                class="el-icon-user-solid"
-                @click="Info"
-                style="font-size: 15px"
-                >&nbsp;信息维护</span
-              ></el-dropdown-item
-            >
-            <el-dropdown-item
-              ><span
-                class="el-icon-s-help"
-                @click="changehead"
-                style="font-size: 15px"
-                >&nbsp;头像更换</span
-              ></el-dropdown-item
-            >
-            <el-dropdown-item
-              ><span
-                class="el-icon-switch-button"
-                @click="SignOut"
-                style="font-size: 15px"
-                >&nbsp;退出登录</span
-              ></el-dropdown-item
-            >
-          </el-dropdown-menu>
-        </el-dropdown>
       </el-aside>
 
       <el-main>
